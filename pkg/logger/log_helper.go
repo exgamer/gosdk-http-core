ce8933fe26d89625d4c3e411c7a8fc3d587267e5
olzhaskulzhambekov <okulzhambekov@jmart.kz>
2026-01-08 12:56:11 +0500

    init

--- pkg/logger/log_helper.go
+++ pkg/logger/log_helper.go
@@ -2,38 +2,20 @@ package logger
 
 import (
 	"github.com/exgamer/gosdk-core/pkg/config"
+	"github.com/exgamer/gosdk-core/pkg/logger"
 	config2 "github.com/exgamer/gosdk-http-core/pkg/config"
-	"log"
-	"os"
 	"strconv"
 	"strings"
-	"time"
 )
 
 // FormattedInfo Форматированный лог
 func FormattedInfo(serviceName string, method string, uri string, status int, requestId string, message string) {
-	FormattedLog("INFO", serviceName, method, uri, status, requestId, message)
+	logger.Info(getFormattedMessage(serviceName, method, uri, status, requestId, message))
 }
 
 // FormattedError Форматированный лог ошибки
 func FormattedError(serviceName string, method string, uri string, status int, requestId string, message string) {
-	FormattedLog("ERROR", serviceName, method, uri, status, requestId, message)
-}
-
-// FormattedLog Форматированный лог
-func FormattedLog(level string, serviceName string, method string, uri string, status int, requestId string, message string) {
-	log.SetOutput(os.Stdout)
-	log.SetFlags(0)
-
-	messageBuilder := strings.Builder{}
-	messageBuilder.WriteString(time.Now().Format("2006-01-02 15:04:05.345"))
-	messageBuilder.WriteString(" " + level + " ")
-	messageBuilder.WriteString("[" + serviceName + "," + requestId + "]")
-	messageBuilder.WriteString("[" + method + "," + uri + "," + strconv.Itoa(status) + "]")
-	messageBuilder.WriteString(" " + message)
-
-	log.Println(messageBuilder.String())
-	log.SetFlags(log.Ldate | log.Ltime)
+	logger.Error(getFormattedMessage(serviceName, method, uri, status, requestId, message))
 }
 
 // FormattedLogWithAppInfo Форматированный лог для RequestData
@@ -44,4 +26,13 @@ func FormattedLogWithAppInfo(appInfo *config.AppInfo, httpInfo *config2.HttpInfo
 // FormattedErrorWithAppInfo Форматированный лог ошибки для RequestData
 func FormattedErrorWithAppInfo(appInfo *config.AppInfo, httpInfo *config2.HttpInfo, message string) {
 	FormattedError(appInfo.ServiceName, httpInfo.RequestMethod, httpInfo.RequestUrl, 1, httpInfo.RequestId, message)
+}
+
+func getFormattedMessage(serviceName string, method string, uri string, status int, requestId string, message string) string {
+	messageBuilder := strings.Builder{}
+	messageBuilder.WriteString("[" + serviceName + "," + requestId + "]")
+	messageBuilder.WriteString("[" + method + "," + uri + "," + strconv.Itoa(status) + "]")
+	messageBuilder.WriteString(" " + message)
+
+	return messageBuilder.String()
 }
